4963cd2b6b8b1000cd2b199469c826dfcb43941f
Le-Yo <lenykoskey@yahoo.com>
2015-07-01 20:15:26 +0300

    added form2.php

--- connect.php
+++ connect.php
@@ -1,10 +1,11 @@
 <?php
 
 $hostname = "localhost";
-$username = "leo";//root
-$password = "12345";//""
+$username = "root";//root
+$password = "";//""
 $connection =  mysql_connect($hostname, $username, $password) or trigger_error(mysql_error(),E_USER_ERROR);
 mysql_select_db("online_airtime");
+
 
 
 ?>
